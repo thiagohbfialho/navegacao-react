835edf0817683f3c01d2e83d76e0e17d4afca0fc
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-03-28 10:16:42 -0300

    React router - Navegação simples

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,9 +1,16 @@
 import './Content.css'
-import React from "react";
+import React from "react"
+import { Routes, Route } from 'react-router-dom'
+
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
 
 const Content = props => (
     <main className="Content">
-        <h1>Componente Content</h1>
+        <Routes>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
     </main>
 )
 
