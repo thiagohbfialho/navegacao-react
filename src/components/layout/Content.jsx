1988360191d75d94c4901fc6374e4e6be7e29d21
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-03-28 10:53:31 -0300

    React router - página não encontrada

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
 import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
 
 const Content = props => (
     <main className="Content">
@@ -12,6 +13,7 @@ const Content = props => (
           <Route path='/about' element={<About/>}/>
           <Route path='/param/:id' element={<Param/>}/>
           <Route path='/' element={<Home/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
     </main>
 )
