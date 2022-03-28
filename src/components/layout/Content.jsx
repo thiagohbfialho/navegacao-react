af548c4fef12180922d393465577d9a21244442d
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-03-28 10:43:14 -0300

    React router - Navegando com parâmetro!

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
 
 const Content = props => (
     <main className="Content">
         <Routes>
           <Route path='/about' element={<About/>}/>
+          <Route path='/param/:id' element={<Param/>}/>
           <Route path='/' element={<Home/>}/>
         </Routes>
     </main>
